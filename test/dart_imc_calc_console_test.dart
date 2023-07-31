52d805752f2c4b30e553840d281f7655d0e20276
Pitossomo <pitossomo@gmail.com>
2023-07-31 06:22:11 -0300

    feat(inputs): Add default test

--- test/dart_imc_calc_console_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_imc_calc_console/dart_imc_calc_console.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
