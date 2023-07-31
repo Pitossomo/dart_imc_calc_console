93000271a00970da3beb21e5ab6f013b3c79c32f
Pitossomo <pitossomo@gmail.com>
2023-07-31 06:21:32 -0300

    feat: Read inputs

--- bin/dart_imc_calc_console.dart
+++ bin/dart_imc_calc_console.dart
@@ -1,7 +1,11 @@
 import 'package:dart_imc_calc_console/classes/pessoa.dart';
+import 'package:dart_imc_calc_console/console_utils.dart';
 
 void main(List<String> arguments) {
   print("*** Calculadora de IMC ***");
 
-  print(Pessoa(73, 163));
+  int height = readInt("altura em cm"); 
+  int weight = readInt("peso em kg");
+
+  print(Pessoa(weight, height));
 }
